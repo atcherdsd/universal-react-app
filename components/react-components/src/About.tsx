4613009fca97ed9cf599398c90975b116bfb1496
atcherdsd <tchernak.csg@gmail.com>
2022-10-01 12:56:23 +0300

    refactor: add destructuring to About

--- components/react-components/src/About.tsx
+++ components/react-components/src/About.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import about from './data/about.json';
 import './About.css';
 
+const [aboutData] = about.aboutData.map((elem) => elem);
+
 function About() {
   return (
     <>
       <h2 className="About-title">About Us</h2>
-      <p className="About-text">{about.aboutData.map((elem) => elem.description1)}</p>
+      <p className="About-text">{aboutData.description[0]}</p>
       <hr className="About-line" />
-      <p className="About-text">{about.aboutData.map((elem) => elem.description2)}</p>
+      <p className="About-text">{aboutData.description[1]}</p>
     </>
   );
 }
