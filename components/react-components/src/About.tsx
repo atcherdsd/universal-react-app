5184acadeeb8a39bb8cfe79c6c7c466be7697f53
atcherdsd <tchernak.csg@gmail.com>
2022-10-01 13:13:54 +0300

    fix: fix destructuring into About

--- components/react-components/src/About.tsx
+++ components/react-components/src/About.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 import about from './data/about.json';
 import './About.css';
 
-const [aboutData] = about.aboutData.map((elem) => elem);
+const [aboutData1, aboutData2] = about.aboutData;
 
 function About() {
   return (
     <>
       <h2 className="About-title">About Us</h2>
-      <p className="About-text">{aboutData.description[0]}</p>
+      <p className="About-text">{aboutData1.description}</p>
       <hr className="About-line" />
-      <p className="About-text">{aboutData.description[1]}</p>
+      <p className="About-text">{aboutData2.description}</p>
     </>
   );
 }
