3b34f71c40cff5f7d6acad2d972f76213c88c113
atcherdsd <tchernak.csg@gmail.com>
2022-10-10 12:36:05 +0300

    init: start react-forms task

---
+++ components/react-components/src/pages/Forms/Forms.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+// import './About.css';
+
+// interface IAboutProps {
+//   p1: string;
+//   p2: string;
+// }
+
+const Forms: React.FC = () => {
+  return (
+    <>
+      <h2 className="Forms-title">This is page for forms</h2>
+    </>
+  );
+};
+
+export default Forms;
