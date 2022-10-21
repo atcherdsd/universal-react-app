97fba0e3913e0b3a64d305986ca713e28be8a1c0
atcherdsd <tchernak.csg@gmail.com>
2022-10-21 22:37:40 +0300

    feat: add tests for Modal component

--- src/components/Modal/Modal.test.tsx
+++ src/components/Modal/Modal.test.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import Modal from './Modal';
+// import SearchBar from 'components/SearchBar/SearchBar';
+// import SearchResult from 'components/SearchResult/SearchResult';
 
 describe('Modal component', () => {
-  beforeEach(() => {
-    render(
+  test('should render Modal component', () => {
+    const snapshot = render(
       <Modal
         data={{
           author: '',
@@ -21,43 +23,38 @@ describe('Modal component', () => {
         handleResult={() => void {}}
       />
     );
+    expect(snapshot).toMatchSnapshot();
   });
+  it('onClick works', () => {
+    const { unmount } = render(
+      <Modal
+        data={{
+          author: '',
+          title: '',
+          description: '',
+          urlToImage: '',
+          publishedAt: '',
+          source: {
+            id: '',
+            name: '',
+          },
+          url: '',
+        }}
+        handleResult={() => void {}}
+      />
+    );
+    const exit = document.querySelector('.Modal-exit') as HTMLDivElement;
+    exit.onclick = jest.fn();
+    const onClick = exit.onclick;
 
-  test('should render Modal component', () => {
-    // expect(screen.getByRole('banner')).toBeInTheDocument();
-    // expect(screen.getAllByRole('heading')[0]).toBeInTheDocument();
-    // expect(screen.getByRole('navigation')).toBeInTheDocument();
-    // expect(screen.getAllByRole('list')[0]).toBeInTheDocument();
-    // expect(screen.getAllByRole('list').length).toEqual(1);
-    // expect(screen.getByAltText('logo')).toBeInTheDocument();
-    // const nav = screen.getAllByRole('navigation');
-    // expect(nav.length).toEqual(1);
-    // const links = document.querySelectorAll('.Header-link');
-    // expect(links.length).toBeGreaterThanOrEqual(4);
-    // const main = screen.getByText(/main/i);
-    // expect(main).toBeInTheDocument();
-    // const forms = screen.getByText(/forms/i);
-    // expect(forms).toBeInTheDocument();
-    // const aboutUs = screen.getByText(/about us/i);
-    // expect(aboutUs).toBeInTheDocument();
-    // const api = screen.getByText(/api/i);
-    // expect(api).toBeInTheDocument();
-  });
-  it('Classes are available', () => {
-    // expect(screen.getByRole('banner')).toHaveClass('Header-container');
-    // expect(screen.getAllByRole('heading')[0]).toHaveClass('Headear-title');
-    // expect(screen.getAllByRole('list')[0]).toHaveClass('Header-links');
-  });
-  it('Styles are available', () => {
-    // const navContainer = document.querySelector('.Header-right') as Element;
-    // const navContainerHeight = window.getComputedStyle(navContainer).height;
-    // expect(navContainerHeight).toBeDefined();
+    expect(screen.queryByText('See in source')).toBeInTheDocument();
+    fireEvent.click(exit);
+    expect(onClick).toHaveBeenCalledTimes(1);
+    unmount();
+    expect(screen.queryByText('See in source')).not.toBeInTheDocument();
   });
   test('Modal component do not have data', () => {
     expect(screen.queryByText(/home/i)).toBeNull();
     expect(screen.queryByRole('paragraph')).toBeNull();
-    // const elementList = document.querySelector('.Header-links') as Element;
-    // const textColor = window.getComputedStyle(elementList).color;
-    // expect(textColor).toBeFalsy();
   });
 });
