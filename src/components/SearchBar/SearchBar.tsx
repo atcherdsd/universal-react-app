0ddb6ec04cab1bd4fa68acd2c4bc0b6b9e695ea6
atcherdsd <tchernak.csg@gmail.com>
2022-10-22 11:35:47 +0300

    feat: add server response error handling

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -17,6 +17,22 @@ export type Data = {
   publishedAt: string;
 };
 
+enum StatusCode {
+  BadRequest = '400',
+  Unauthorized = '401',
+  Forbidden = '403',
+  TooManyRequests = '429',
+  InternalServerError = '500',
+}
+enum ErrorMessage {
+  BadRequest = 'Your request is not valid. Please fill in the search field and try again',
+  Unauthorized = 'Your API key is incorrect. Use a valid key',
+  Forbidden = 'You have reached your daily request limit, the next reset is at 00:00 UTC',
+  TooManyRequests = 'You have made more requests per second than you are allowed',
+  InternalServerError = 'We had a problem with our server. Try again later',
+  AnotherError = 'Server error. Try again later',
+}
+
 const basicURL = 'https://gnews.io/api/v4/search';
 const KEY = 'b1a198162ce907ddfd42b009b63ab35e';
 // const KEY = 'd42b009b63ab35e';
@@ -48,10 +64,19 @@ function SearchBar(): JSX.Element {
 
     try {
       const response = await fetch(`${basicURL}?token=${KEY}&q=${searchValue}`);
-      if (response.status.toString() === '400')
-        throw new Error('Your request is not valid. Please fill in the request field');
-      else if (!response.ok) throw new Error('Server error. Please try again later');
-
+      switch (response.status.toString()) {
+        case StatusCode.BadRequest:
+          throw new Error(ErrorMessage.BadRequest);
+        case StatusCode.Unauthorized:
+          throw new Error(ErrorMessage.Unauthorized);
+        case StatusCode.Forbidden:
+          throw new Error(ErrorMessage.Forbidden);
+        case StatusCode.TooManyRequests:
+          throw new Error(ErrorMessage.TooManyRequests);
+        case StatusCode.InternalServerError:
+          throw new Error(ErrorMessage.InternalServerError);
+      }
+      if (!response.ok) throw Error(ErrorMessage.AnotherError);
       const data: IContentItem = await response.json();
       setContentItem(data);
     } catch (err) {
